cf8772ee1c2dd9e081f03b4027c0a7d085fcd62b
François Georgy <francois.georgy@gmail.com>
2021-10-12 13:28:56 +0200

    Project creation

---
+++ README.md
@@ -0,0 +1,11 @@
+DISTINGuished
+=============
+
+### A WebMIDI utility for the Expert Sleepers Disting Mk4
+
+Doc:
+
+- https://www.expert-sleepers.co.uk/disting.html
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                Distinguished - A webmidi utility for the Expert Sleepers Disting Mk4
+            </header>
+        </div>
+    )
+}
+
+export default App
